4c2beed1dee00c77087690c3a44c671550a73889
yusof <moyusof05@gmail.com>
2026-08-28 12:26:12 +0800

    Modify Terraform config

---
+++ terraform/output.tf
@@ -0,0 +1,15 @@
+
+output "node1_private_ip" { value = module.node1.private_ip }
+output "node2_private_ip" { value = module.node2.private_ip }
+output "node3_private_ip" { value = module.node3.private_ip }
+output "node3_public_ip" { value = module.node3.public_ip }
+
+
+output "ssm_node1" {value = "aws ssm start-session --target ${module.node1.id}"}
+output "ssm_node2" {value = "aws ssm start-session --target ${module.node2.id}"}
+output "ssm_node3" {value = "aws ssm start-session --target ${module.node3.id}"}
+
+
+
+
+
--- terraform/userdata.sh
+++ terraform/userdata.sh
@@ -2,5 +2,4 @@
 curl -fsSL https://get.docker.com | sh
 id ssm-user &>/dev/null || useradd -m ssm-user
 usermod -aG docker ssm-user
-docker run -d --network host cloudflare/cloudflared:latest \
-  tunnel --no-autoupdate run --token ${tunnel_token}
+docker run -d -p 80:80 nginx
